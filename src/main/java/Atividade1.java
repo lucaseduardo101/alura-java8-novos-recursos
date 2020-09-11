99d8d57db0073ca51f4fbb2fe4b55c8d6e9c49e5
lucas.oliveira <lucas.oliveira@visagio.com>
2020-09-10 23:55:13 -0300

    feat: first activitie

---
+++ src/main/java/Atividade1.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class Atividade1{
+    public static void main(String[]args){
+        List<String> palavras = new ArrayList<>();
+        palavras.add("alura online");
+        palavras.add("casa do código");
+        palavras.add("caelum");
+
+        palavras.sort(new ComparadorDeStringPorTamanho());
+
+        Consumer<String> consumidor = new ConsumidorDeString();
+        palavras.forEach(consumidor);
+    }
+}
+class ComparadorDeStringPorTamanho implements Comparator<String> {
+    public int compare(String s1, String s2) {
+        if(s1.length() < s2.length())
+            return -1;
+        if(s1.length() > s2.length())
+            return 1;
+        return 0;
+    }
+}
+
+class ConsumidorDeString implements Consumer<String> {
+    public void accept(String s) {
+        System.out.println(s);
+    }
+}
+
